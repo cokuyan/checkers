e60362caf2748dc5344ed682ab81c6d18fa085df
Cihangir Okuyan <chiryoku@gmail.com>
2014-11-20 12:03:47 -0500

    Debug perform_jump and promotable?, and create moves method for piece class

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,5 +1,6 @@
 require_relative 'board.rb' #just in case
 
+class CheckersError < StandardError; end
 class InvalidMoveError < CheckersError; end
 
 class Piece
@@ -11,11 +12,11 @@ class Piece
     [1, -1]
   ]
 
-  attr_reader :pos, :color
+  attr_reader :pos, :color, :deltas
 
   def initialize(board, color, pos)
     @board, @color, @pos = board, color, pos
-    @deltas = if @color == :red
+    @deltas = if @color == :white
                 DELTAS.map { |row| [row.first * -1, row.last] }
               else
                 DELTAS
@@ -34,15 +35,24 @@ class Piece
 
   def move_diffs
     if promoted?
-      DELTAS + DELTAS.map{ |row| [row.first * -1, row.last] }
+      @deltas + @deltas.map{ |row| [row.first * -1, row.last] }
     else
-      DELTAS
+      @deltas
     end
+  end
+
+  def moves(move)
+    move_diffs
+    moves = []
+    multiplier = move == :slide ? 1 : 2
+    x, y = pos
+
+    @deltas.map { |dx, dy| [x + dx * multiplier, y + dy * multiplier] }
   end
 
   def perform_slide(slide_pos)
     slide_pos.each_index do |i|
-      raise InvalidMoveError unless (slide_pos[i] - pos[i]).abs == 1
+      raise InvalidMoveError unless moves(:slide).include?(slide_pos)
     end
     return false unless @board[slide_pos].nil?
     @board.remove_piece(pos)
@@ -55,12 +65,14 @@ class Piece
 
   def perform_jump(jump_pos)
     jump_pos.each_index do |i|
-      raise InvalidMoveError unless (jump_pos[i] - pos[i]).abs == 2
+      raise InvalidMoveError unless moves(:jump).include?(jump_pos)
     end
     adjacent_pos = [(pos.first + jump_pos.first) / 2,
                     (pos.last + jump_pos.last) / 2]
 
-    return false unless @board[adjacent_pos] && @board[jump_pos].nil?
+    return false unless @board[adjacent_pos] &&
+                        @board[jump_pos].nil? &&
+                        @board[adjacent_pos].color != self.color
 
     @board.remove_piece(pos)
     @board.remove_piece(adjacent_pos)
@@ -81,7 +93,7 @@ class Piece
   def promotable?
     row = pos.first
 
-    color == :white ? row == 7 : row == 0
+    color == :white ? row == 0 : row == 7
   end
 
   def render
