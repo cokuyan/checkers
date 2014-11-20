93225d71ebd5b04880d78253d7bb6a0d30c7c4cc
Cihangir Okuyan <chiryoku@gmail.com>
2014-11-20 10:11:06 -0500

    Implement single moves and promotion

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,10 +1,44 @@
 class Piece
 
-  def intialize(board, pos)
-    @board, @pos = board, pos
+  # may remove later
+  # iterate over it twice for king positions also
+  DELTAS = [
+    [1, 1]
+    [1, -1]
+  ]
+
+  attr_reader :pos, :color
+
+  def intialize(board, pos, color)
+    @board, @pos, @color = board, pos, color
 
     board.place_piece(self, @pos)
     @promoted = false
+  end
+
+
+  def perform_slide(slide_pos)
+    return false unless @board[slide_pos].nil?
+    @board.remove_piece(pos)
+    @pos = slide_pos ##### Put this into board class?
+    #### when placing a piece, remove it from board in old position?
+    @board.place_piece(self, slide_pos)
+    promote if promotable? && !promoted?
+    true
+  end
+
+  def perform_jump(jump_pos)
+    adjacent_pos = [(pos.first + jump_pos.first) / 2,
+                    (pos.last + jump_pos.last) / 2]
+
+    return false unless @board[adjacent_pos] && @board[jump_pos].nil?
+
+    @board.remove_piece(pos)
+    @board.remove_piece(adjacent_pos)
+    @board.place_piece(self, jump_pos)
+    @pos = jump_pos ### same note as perform_slide
+    promote if promotable? && !promoted?
+    true
   end
 
   def promote
@@ -15,11 +49,10 @@ class Piece
     @promoted
   end
 
-  def perform_slide
-  end
+  def promotable?
+    row = pos.first
 
-  def perform_jump
+    color == :white ? row == 7 : row == 0
   end
-
 
 end
